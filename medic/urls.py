e2af99f68ed4b8485effd926d1f2177dc392231f
Faith-qa <atienofaith12@gmail.com>
2021-06-06 16:23:55 +0300

    changes

--- medic/urls.py
+++ medic/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from medic import views
 
 urlpatterns = [
-  path(' ', views.MedicList),
-  path('<int:pk>/', views.MedicDetail),
+  path('', views.MedicList.as_view()),
+  path('<int:pk>/', views.MedicDetail.as_view()),
 ]
