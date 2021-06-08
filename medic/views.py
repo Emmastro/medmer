4b554fdd9e566119b8c430bdb5236f94fd1dd3ba
Faith-qa <atienofaith12@gmail.com>
2021-06-08 17:40:55 +0300

    changes

--- medic/views.py
+++ medic/views.py
@@ -4,13 +4,29 @@ from medic.serializers import MedicSerializer
 from rest_framework import generics
 from rest_framework import permissions
 from medic.permissions import IsOwnerOrReadOnly
+from rest_framework.response import Response
+from rest_framework.exceptions import AuthenticationFailed
 
 
 class MedicList(generics.ListCreateAPIView):
     queryset = Medic.objects.all()
     serializer_class = MedicSerializer
     # TODO: implement and test authentication to uncomment the next line
-    # permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    def post(self, request):
+        email = request.data['email']
+        password = request.data['password']
+        
+        user = Medic.objects.filter(email=email).first()
+
+        if user is None:
+            raise AuthenticationFailed('user not found!')
+        
+        if not user.check_password(password):
+            raise AuthenticationFailed('Incorrect password!')
+        
+        return Response(user)
+
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save()
