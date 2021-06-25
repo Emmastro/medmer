719beeb4a400892aef8347dbdbda3b017753dc03
Emmanuel Murairi <emurairi18@alastudents.org>
2021-06-25 16:16:38 +0400

    setup help request list and detail page for medic

--- helprequest/models.py
+++ helprequest/models.py
@@ -7,12 +7,15 @@ class HelpRequest(models.Model):
     # TODO: medic should be able to perform help requests too
     patient = models.ForeignKey('patient.Patient', on_delete=models.CASCADE)
     
-    medic = models.ForeignKey('medic.Medic', on_delete=models.CASCADE)
-    medic_notes = models.TextField()
+    medic = models.ForeignKey('medic.Medic', on_delete=models.CASCADE, blank=True, null=True)
+    medic_notes = models.TextField(blank=True, null=True)
     
-    patient_notes = models.TextField()
-    patient_location = models.CharField(max_length=120)
+    patient_notes = models.TextField(blank=True, null=True)
+    patient_location = models.CharField(max_length=120, blank=True, null=True)
 
     time_requested = models.DateTimeField(auto_now_add=True)
-    time_accepted = models.DateTimeField()
-    time_concluded = models.DateTimeField()
+    time_accepted = models.DateTimeField(blank=True, null=True)
+    time_concluded = models.DateTimeField(blank=True, null=True)
+
+    def __str__(self):
+        return "{}-{}-{}".format(self.id, self.patient, self.time_requested)
--- helprequest/views.py
+++ helprequest/views.py
@@ -1,60 +1,36 @@
+from django.utils.decorators import method_decorator
+
+from django.contrib.auth.decorators import login_required
+
+from django.views.generic import ListView, DetailView, UpdateView, CreateView
+
 from helprequest.models import HelpRequest
-from patient.serializers import PatientSerializer
-from helprequest.serializers import HelpRequestSerializer
-from rest_framework import generics
-from rest_framework import permissions
-from rest_framework.response import Response
 
 
-class HelpRequestList(generics.ListCreateAPIView):
+@method_decorator(login_required, name='dispatch')
+class HelpRequestList(ListView):
+
+	model = HelpRequest
+	template_name = "help_request_list.html"
+	context_object_name = "help_requests"
+	paginate_by = 20
+	order_by="category"
+	
+	
+	"""def get_queryset(self):
+		
+		key = self.request.GET.get('key', None)
+		if  key!= None:
+			object_list = self.model.objects.filter(name__icontains = key)
+		else:
+			object_list = self.model.objects.order_by(self.order_by)
+		
+		return object_list
     """
-    Get: returns the list of help requests of a given patient \n
-    Post: save a new help request
-    """
 
-    serializer_class = HelpRequestSerializer
-    #permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-    queryset = HelpRequest.objects.all()
+@method_decorator(login_required, name='dispatch')
+class HelpRequestDetail(DetailView):
 
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-
-class PatientHelpRequestList(generics.ListCreateAPIView):
-    """returns the list of help requests of a given patient"""
-
-    serializer_class = HelpRequestSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def get(self, request, patient_id):
-        queryset = HelpRequest.objects.get(patient__id=patient_id)
-        return Response(queryset)
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-
-class MedicHelpRequestList(generics.ListCreateAPIView):
-    """returns the list of help requests of a given medic"""
-    queryset = HelpRequest.objects.all()
-    serializer_class = HelpRequestSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def get(self, request, medic_id):
-        queryset = HelpRequest.objects.get(medic__id=medic_id)
-        return Response(queryset)
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-    
-
-
-class HelpRequestDetail(generics.RetrieveUpdateDestroyAPIView):
-
-    serializer_class = PatientSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
-
-    def get(self, request, pk):
-        """returns details of a specific help request """
-        queryset = HelpRequest.objects.get(pk=pk)
-        return Response(queryset)
+    model = HelpRequest
+    template_name = "help_request_detail.html"
+    context_object_name = "help_request"
