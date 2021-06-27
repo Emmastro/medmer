a7d5df00f4b3024b69c1a6d91b99a831b6edb6ac
Faith-qa <atienofaith12@gmail.com>
2021-06-27 21:12:00 +0300

    implementing form.py

--- helprequest/views.py
+++ helprequest/views.py
@@ -8,19 +8,16 @@ from helprequest.models import HelpRequest
 
 from helprequest.forms import HelpRequestForm
 
+from django.shortcuts import render, redirect
 
 @method_decorator(login_required, name='dispatch')
 class HelpRequestList(ListView):
-	queryset = HelpRequest.objects.all()
-	form_class = HelpRequestForm
-	print(form_class)
 
-
-
-	#context_object_name = "help_requests"
-	#paginate_by = 20
-		
-
+	model = HelpRequest
+	template_name = "help_request_list.html"
+	context_object_name = "help_requests"
+	paginate_by = 20
+	order_by="category"
 	
 	
 	"""def get_queryset(self):
@@ -41,18 +38,18 @@ class HelpRequestDetail(DetailView):
     template_name = "help_request_detail.html"
     context_object_name = "help_request"
 
-#@method_decorator(login_required, name='dispatch')
-#def PatientHelp(request):
-    #form_class = HelpRequestForm
-    #form  = form_class(request.POST or None)
-    #if request.method == 'POST':
+@method_decorator(login_required, name='dispatch')
+def gethelp(request):
+    form_class = HelpRequestForm
+    form  = form_class(request.POST or None)
+    if request.method == 'POST':
         #form = MedicForm(request.POST, request.FILES)
         
-        #if form.is_valid():
+        if form.is_valid():
 
-             #form.save()
-            # return redirect('home')
+             form.save()
+             return redirect('home')
              
             
-    #context = {'form': form}
-    #return render(request, 'help_request.html', context)
+    context = {'form': form}
+    return render(request, 'help_request.html', context)
