77337741e558f5f9cc979cc88e4d031e4eaf7d18
Faith-qa <atienofaith12@gmail.com>
2021-07-01 16:27:32 +0300

    made change

--- helprequest/views.py
+++ helprequest/views.py
@@ -52,18 +52,7 @@ class RequestHelp(FormView):
     template_name = 'help_request.html'
     form_class = HelpRequestForm
     success_url = 'status'
-    def gethelp(request):
-        form = form_class(request.POST or None)
-        if request.method == 'POST':
-             if form.is_valid():
-
-                form.save()
-                User = HelpRequest.patient
-                return redirect('help_request_status.html')
-        context = {'form': form}
-        return render(request, 'help_request.html', context)
-          
-
+   
     def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
         # It should return an HttpResponse.
@@ -93,14 +82,7 @@ class HelpResponse(FormView):
     form_class = HelpResponseForm
     print(form_class)
     success_url = 'success'
-def givehelp(request):
-    if request.method == 'POST':
-        if form.is_valid():
 
-            form.save()
-            User = HelpRequest.medic
-            return redirect('home')
-        
 
 def form_valid(self, form):
         # This method is called when valid form data has been POSTed.
