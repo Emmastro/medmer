62f5aae0709d5df781ba674ec491ab568924823c
Emmanuel Murairi <emurairi18@alastudents.org>
2021-07-07 19:52:04 +0400

    fix link error and review design

--- home/urls.py
+++ home/urls.py
@@ -8,11 +8,7 @@ from django.contrib.auth.views import PasswordResetView, \
 
 urlpatterns = [
 	path('', views.home, name='home'),
-	path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
-	path('password_reset/', PasswordResetView.as_view(template_name='registration/password_reset.html'), name='password_reset' ),
-	path('password_reset/done/',  PasswordResetDoneView.as_view(template_name='registration/password_reset_success.html'), name='password_reset_done'),
-	path('reset/<uidb64>/<token>/', PasswordResetConfirmView.as_view(template_name='registration/password_reset_set.html'), name='password_reset_confirm'),
-	path('reset/done/', PasswordResetCompleteView.as_view(template_name='registration/password_reset_completed.html'), name='password_reset_complete'),
+	path('about/', views.about, name='about'),
 	path('accounts/profile/', views.profile, name='profile'),
-	
+	path('logout/', LogoutView.as_view(template_name='registration/logout.html'), name='logout'),
 ]
